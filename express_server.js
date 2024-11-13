1df68ad094ac2b7128e3d567de4a1d1413316ee0
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-11-12 17:22:10 -0700

    storing new long and short url in the dabatase and redirecting urls

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ const urlDatabase = {
 };
 
 function generateRandomString() {
-  
+  return 'ABCDE'
 };
 
 app.use(express.urlencoded({ extended: true }));
@@ -30,11 +30,19 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const newURL = generateRandomString();
+  urlDatabase[newURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${newURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
